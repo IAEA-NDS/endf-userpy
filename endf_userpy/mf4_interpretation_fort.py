107c4920032dfe80b2385fc420d4c918efdb9c51
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-09-22 09:28:35 +0200

    refactor: move Fortran wrapper `get_angdist_from_legendre` to new file

---
+++ endf_userpy/mf4_interpretation_fort.py
@@ -0,0 +1,76 @@
+import numpy as np
+from .fortran.endf6 import mf4_get_leg
+from .interpolation import (
+    find_interval,
+    convert_interp_repr,
+)
+from .properties import (
+    get_AWI, get_AWR, get_AWP,
+    get_QM, get_QI, get_LR,
+)
+from .helpers import dict2array
+
+
+def get_angdist_from_legendre(endf_dict, mt, energies, angle_cosines):
+    mf4sec = endf_dict[4][mt]
+    awi = get_AWI(endf_dict)
+    awr = get_AWR(endf_dict)
+    awp = get_AWP(endf_dict, mt)
+    lct = mf4sec['LCT']
+    qm = get_QM(endf_dict, mt)
+    qi = get_QI(endf_dict, mt)
+    breakup_flag = get_LR(endf_dict, mt)
+    q = qi if breakup_flag == 0 else qm
+
+    # get the energy mesh and bookkeeping information
+    incident_energies = dict2array(mf4sec['E'])
+    nbt_arr = np.array(mf4sec['NBT'])
+    int_arr = np.array(mf4sec['INT'])
+    interp_arr = convert_interp_repr(int_arr, nbt_arr)
+    num_coeffs_per_energy = dict2array(mf4sec['NL'])
+    max_num_coeffs = np.max(num_coeffs_per_energy)
+    # convert Legendre coefficients to numpy array
+    coeffs = mf4sec['a']
+    coeffs_arr = np.zeros((len(incident_energies), max_num_coeffs), dtype=float)
+    for en_idx in range(len(incident_energies)):
+        num_coeffs = num_coeffs_per_energy[en_idx]
+        coeffs_arr[en_idx, 0:num_coeffs] = dict2array(coeffs[en_idx+1])
+    # find enclosing energy intervals
+    idcs = find_interval(incident_energies, energies)
+
+    # call the fortran routine
+    result_dim = (len(energies), len(angle_cosines))
+    result_arr = np.zeros(result_dim, dtype=float)
+    for i in range(result_arr.shape[0]):
+        curidx = idcs[i]
+        en1 = incident_energies[curidx]
+        en2 = incident_energies[curidx+1]
+        coeffs1 = coeffs_arr[curidx]
+        coeffs2 = coeffs_arr[curidx+1]
+        num_coeffs1 = num_coeffs_per_energy[curidx]
+        num_coeffs2 = num_coeffs_per_energy[curidx+1]
+        interp_type = interp_arr[curidx]
+
+        # TODO: group all energies falling in a certain bin
+        #       together and pass them all at once to mf4_get_leg
+        num_energies = 1
+        num_angles = len(angle_cosines)
+        energy_out = np.array([energies[i]], dtype=float)
+        cur_res_arr = np.zeros((num_energies, result_dim[1]) , dtype=float)
+
+        # call the fortran routine
+        # NOTE: num_energies comment out because f2py
+        #       derives ne argument from shape of f4
+        cur_res = mf4_get_leg(
+            awr, awi, awp, q, lct,
+            en1, coeffs1, num_coeffs1,
+            en2, coeffs2, num_coeffs2,
+            interp_type,
+            energy_out, #  num_energies, (removed because automatically inferred)
+            angle_cosines, num_angles,
+            cur_res_arr
+        )
+        # copy current result to overall result array
+        result_arr[i:i+1,:] = cur_res_arr
+
+    return cur_res_arr
