90c1c78c2a867573ccf22f5c517a6e327a051ddc
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-09-24 18:20:59 +0200

    add test to check mf4/mt2 LTT=2, LI=0 case

--- tests/test_mf4_interpretation.py
+++ tests/test_mf4_interpretation.py
@@ -16,3 +16,15 @@ def test_mf4_legrepr_python_fortran_equivalence():
     res_py = mf4py.compute_angdist(endf_dict, 2, energies, angcos)
     res_fort = mf4fort.compute_angdist(endf_dict, 2, energies, angcos)
     assert np.allclose(res_py, res_fort)
+
+
+def test_mf4_tabulated_python_fortran_equivalence():
+    parser = EndfParserCpp(ignore_missing_tpid=True)
+    data_dir = Path(__file__).resolve().parent / 'data'
+    endf_file = data_dir / 'jeff33_13-Al-27g_mf4_mt2.endf'
+    endf_dict = parser.parsefile(endf_file)
+    energies = np.array([1e6, 2e6, 3e6])
+    angcos = np.cos(deg2rad(np.linspace(0.0, 180.0, 5)))
+    res_py = mf4py.compute_angdist(endf_dict, 2, energies, angcos)
+    res_fort = mf4fort.compute_angdist(endf_dict, 2, energies, angcos)
+    assert np.allclose(res_py, res_fort)
