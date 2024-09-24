216217396f2106d86da008d8ed11ab72d78f067e
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-09-24 18:18:08 +0200

    implement Python `compute_angdist_from_tabulated` fun

--- endf_userpy/mf4_interpretation.py
+++ endf_userpy/mf4_interpretation.py
@@ -1,6 +1,8 @@
 import numpy as np
 from .interpolation import (
     evaluate_interp_legendre_polynomials,
+    interp,
+    endf_interp1d,
 )
 from .conversion import (
     compute_r2,
@@ -9,7 +11,9 @@ from .conversion import (
 )
 from .helpers import (
     deg2rad,
-    dict2array
+    dict2array,
+    find_interval,
+    convert_interp_repr,
 )
 from .properties import (
     get_AWR, get_AWI, get_AWP,
@@ -41,6 +45,55 @@ def compute_angdist_from_legrepr(mf4sec, energies, angle_cosines):
     return f
 
 
+def compute_angdist_from_tabulated(mf4sec, energies, angle_cosines):
+    mu = angle_cosines
+    if mu.ndim == 1:
+        mu = mu.reshape(1, -1)
+    en_mesh = dict2array(mf4sec['E'], dtype=float)
+    nbt_arr = np.array(mf4sec['energy_table']['NBT'], dtype=int)
+    int_arr = np.array(mf4sec['energy_table']['INT'], dtype=int)
+    idcs = find_interval(en_mesh, energies)
+    interp_arr = convert_interp_repr(int_arr, nbt_arr)
+    # interpolation between angles
+    result_dim = (len(energies), mu.shape[1])
+    result_arr = np.zeros(result_dim, dtype=float)
+    tab1_records = mf4sec['angtable']
+    for i, idx in enumerate(idcs):
+        curtab1 = tab1_records[idx+1]
+        mu_mesh1 = np.array(curtab1['mu'], dtype=float)
+        f_mesh1 = np.array(curtab1['f'], dtype=float)
+        int_arr1 = np.array(curtab1['INT'], dtype=int)
+        nbt_arr1 = np.array(curtab1['NBT'], dtype=int)
+        curtab2 = tab1_records[idx+2]
+        mu_mesh2 = np.array(curtab2['mu'], dtype=float)
+        f_mesh2 = np.array(curtab2['f'], dtype=float)
+        int_arr2 = np.array(curtab2['INT'], dtype=int)
+        nbt_arr2 = np.array(curtab2['NBT'], dtype=int)
+        if mu.shape[0] == 1:
+            curmu = mu[0,:]
+        else:
+            curmu = mu[i,:]
+
+        f1 = endf_interp1d(
+            curmu, mu_mesh1, f_mesh1, int_arr1, nbt_arr1
+        )
+        f2 = endf_interp1d(
+            curmu, mu_mesh2, f_mesh2, int_arr2, nbt_arr2
+        )
+        interp_type = interp_arr[idx]
+        red_en_mesh = en_mesh[idx:idx+2]
+        red_f = np.vstack([f1, f2])
+        curen = energies[i]
+        curres = np.zeros(mu.shape[1], dtype=float)
+        for j in range(mu.shape[1]):
+            curres[j] = \
+                interp(curen, red_en_mesh , red_f[:,j], interp_type)
+
+        result_arr[i,:] = curres
+
+    return result_arr
+
+
 def _compute_r2(endf_dict, mt, energies):
     awi = get_AWI(endf_dict)
     awr = get_AWR(endf_dict)
@@ -69,6 +122,8 @@ def compute_angdist(endf_dict, mt, energies, angle_cosines):
     # perform the appropriate interpolation
     if ltt == 1 and li == 0:
         f_eff = compute_angdist_from_legrepr(mf4sec, energies, mu_eff)
+    elif ltt == 2 and li == 0:
+        f_eff = compute_angdist_from_tabulated(mf4sec, energies, mu_eff)
     else:
         raise ValueError(f'Unknown angular distribution representation.')
     # convert result to LAB system if required
