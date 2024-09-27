60ef5a01ba7285b8e6c556fa1b34ccdea1ec47f1
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-09-27 16:22:30 +0200

    add implementation of `erf` function

--- endf_userpy/helpers.py
+++ endf_userpy/helpers.py
@@ -60,3 +60,25 @@ def get_enclosing_points(x, xp, fp):
     y1 = fp[idcs1]
     y2 = fp[idcs2]
     return x1, y1, x2, y2
+
+
+# Taken from https://stackoverflow.com/a/457805
+# with small adjustments for array compatibility
+def erf(x):
+    # save the sign of x
+    sign = np.ones_like(x, dtype=float)
+    sign[x < 0] = -1.0
+    x = np.abs(x)
+
+    # constants
+    a1 =  0.254829592
+    a2 = -0.284496736
+    a3 =  1.421413741
+    a4 = -1.453152027
+    a5 =  1.061405429
+    p  =  0.3275911
+
+    # A&S formula 7.1.26
+    t = 1.0/(1.0 + p*x)
+    y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*np.exp(-x*x)
+    return sign*y # erf(-x) = -erf(x)
