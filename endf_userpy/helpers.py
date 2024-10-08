0a7fc76507b1cc9dc73c9e1c4f806d94eab5684f
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-10-08 16:31:57 +0200

    generalize `dict2array` for multi-dim arrays

--- endf_userpy/helpers.py
+++ endf_userpy/helpers.py
@@ -6,7 +6,12 @@ def deg2rad(values):
 
 
 def dict2array(obj, dtype=None):
-    return np.array(list(v for v in obj.values()), dtype=dtype)
+    """Construct (multi-dim) array from nested dictionaries"""
+    if isinstance(next(iter(obj.values())), dict):
+        lst = list(dict2array(v, dtype) for v in obj.values())
+    else:
+        lst = list(obj.values())
+    return np.array(lst, dtype=dtype)
 
 
 def check_int_nbt(int_arr, nbt_arr):
