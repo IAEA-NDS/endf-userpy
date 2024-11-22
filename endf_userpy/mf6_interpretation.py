1a92f3c4cee61b96c0368d8a63f6430bf5bf70d1
gschnabel <40870991+gschnabel@users.noreply.github.com>
2024-11-22 16:34:14 +0100

    disable MT/ZAP consistency check and fix a print bug

--- endf_userpy/mf6_interpretation.py
+++ endf_userpy/mf6_interpretation.py
@@ -1,10 +1,12 @@
 import numpy as np
-from .properties import is_zap_consistent
+from .properties import (
+    is_zap_consistent,
+    get_ZAP,
+)
 from .interpolation import interp_tab1
 from .mf6_interpretation_helpers import (
     check_mf6_exists,
     check_mt_exists_in_mf6,
-    get_zap_with_check,
     find_subsec_num,
     contains_subsec_dist2d,
 )
@@ -81,7 +83,7 @@ def compute_dist2d_from_subsec(
 def get_incident_energies(endf_dict, mt, zap):
     check_mf6_exists(endf_dict)
     check_mt_exists_in_mf6(endf_dict, mt)
-    zap = get_zap_with_check(endf_dict, mt, zap)
+    zap = zap if zap is not None else get_ZAP(endf_dict, mt)
     subsec_num = find_subsec_num(endf_dict, mt, zap)
     return get_incident_energies_from_subsec(
         endf_dict, mt, subsec_num
@@ -93,11 +95,12 @@ def compute_dist2d_values(
 ):
     check_mf6_exists(endf_dict)
     check_mt_exists_in_mf6(endf_dict, mt)
-    zap = get_zap_with_check(endf_dict, mt, zap)
+    zap = zap if zap is not None else get_ZAP(endf_dict, mt)
     subsec_num = find_subsec_num(endf_dict, mt, zap)
     if not contains_subsec_dist2d(endf_dict, mt, subsec_num):
+        law = endf_dict[6][mt]['subsection'][subsec_num]['LAW']
         raise ValueError(
-            f'Found product ZAP={zap} in MF6/MT{mt}/subsection[{idx}] '
+            f'Found product ZAP={zap} in MF6/MT{mt}/subsection[{subsec_num}] '
             f'but only an angular distribution is given (LAW={law})'
         )
     return compute_dist2d_from_subsec(
@@ -108,7 +111,7 @@ def compute_dist2d_values(
 def compute_yields(endf_dict, mt, zap, energies_in):
     check_mf6_exists(endf_dict)
     check_mt_exists_in_mf6(endf_dict, mt)
-    zap = get_zap_with_check(endf_dict, mt, zap)
+    zap = zap if zap is not None else get_ZAP(endf_dict, mt)
     subsec_num = find_subsec_num(endf_dict, mt, zap)
     if subsec_num is None:
         raise IndexError(
