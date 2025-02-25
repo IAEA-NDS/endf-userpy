1ad79aabde677c777a2f5449cd5f9f18b5cc5d36
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-02-25 19:47:36 +0100

    add `distribution2d` module

---
+++ endf_userpy/distribution2d.py
@@ -0,0 +1,36 @@
+import numpy as np
+from . import mf4_interpretation_fort as mf4_interp 
+from . import mf5_interpretation as mf5_interp
+from . import mf6_interpretation as mf6_interp 
+from .properties import get_zap_for_particle 
+
+
+def compute_dist2d_values_from_mf4_mf5(endf_dict, mt, zap, energies_in, energies_out, angle_cosines_out, to_lab=True):
+    if to_lab is not True:
+        raise ValueError(
+            f"Double-differential distribution for MT={mt}, ZAP={zap} reconstruction "
+            "from MF4 and MF5 only possible with `to_lab=True` argument."
+        )
+    if zap != get_zap_for_particle('n'):
+        raise ValueError(
+            f"Value of ZAP={zap} does not correspond to a neutron as ejectile. "
+            "Reconstruction from MF4 and MF5 is only possible for neutrons."
+        )
+    energy_spect = mf5_interp.compute_spectrum(
+        endf_dict, mt, energies_in, energies_out
+    )
+    angular_dist = mf4_interp.compute_angdist_values(
+        endf_dict, mt, energies_in, angle_cosines_out, to_lab
+    )
+    energy_spect = energy_spect[:, :, np.newaxis]
+    angular_dist = angular_dist[:, np.newaxis, :]
+    dist2d_values = energy_spect * angular_dist
+    return dist2d_values
+
+
+def compute_dist2d_values_from_mf6(
+    endf_dict, mt, zap, energies_in, energies_out, angle_cosines_out, to_lab=True
+):
+    return mf6_interp.compute_dist2d_values(
+        endf_dict, mt, zap, energies_in, energies_out, angle_cosines_out, to_lab
+    )
