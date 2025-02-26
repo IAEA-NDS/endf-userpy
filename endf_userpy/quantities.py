6c9861f4768fc790c77a3612c399625cc5bbad7a
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-02-26 18:35:41 +0100

    implement yields and 2d distribution for fission MT=18

--- endf_userpy/quantities.py
+++ endf_userpy/quantities.py
@@ -1,12 +1,14 @@
 import numpy as np
 from . import properties
 from . import reactions as reaction
+from .physical_constants import get_zap_for_particle
 from .properties import (
     is_zap_consistent,
     has_mf4_mt,
     has_mf5_mt,
     has_mf6_mt,
 )
+from . import mf1_interpretation as mf1_interp
 from . import mf3_interpretation as mf3_interp
 from . import mf4_interpretation_fort as mf4_interp
 from . import mf6_interpretation as mf6_interp
@@ -27,7 +29,16 @@ def compute_dxs(endf_dict, mt, zap, energies_in, angle_cosines_out, to_lab=True)
 
 
 def compute_yields(endf_dict, mt, zap, energies_in):
-    if has_mf6_mt(endf_dict, mt):
+    if mt == 18:
+        neutron_zap = get_zap_for_particle('n')
+        if zap != neutron_zap:
+            raise ValueError(
+                f'For fission, only yield of emitted neutrons can be computed '
+                f'zap={neutron_zap} but obtained zap={zap}'
+            )
+        # if MT=18 (n,f), we assume user wants to know prompt neutron yields
+        yields = mf1_interp.compute_yields(endf_dict, 456, energies_in)
+    elif has_mf6_mt(endf_dict, mt):
         yields = mf6_interp.compute_yields(endf_dict, mt, zap, energies_in)
     else:
         proj = properties.get_projectile(endf_dict)
