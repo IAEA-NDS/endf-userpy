20287a07aac57b7bcd9cdd7b2275403e761f2dd1
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-02-27 21:55:47 +0100

    use `distribution1d` funcs in `quantities` module

--- endf_userpy/quantities.py
+++ endf_userpy/quantities.py
@@ -1,31 +1,17 @@
 import numpy as np
+from scipy.integrate import quad
 from . import properties
 from . import reactions as reaction
 from .physical_constants import get_zap_for_particle
 from .properties import (
     is_zap_consistent,
-    has_mf4_mt,
-    has_mf5_mt,
     has_mf6_mt,
 )
 from . import mf1_interpretation as mf1_interp
-from . import mf3_interpretation as mf3_interp
-from . import mf4_interpretation_fort as mf4_interp
 from . import mf6_interpretation as mf6_interp
+from .distribution1d import compute_angdist_values
 from .distribution2d import compute_dist2d_values
-
-
-compute_xs = mf3_interp.compute_cross_section
-
-
-def compute_daxs(endf_dict, mt, zap, energies_in, angle_cosines_out, to_lab=True):
-    if not is_zap_consistent(endf_dict, mt, zap):
-        raise ValueError(f'MT={mt} and ZAP={mt} are not consistent')
-    angdist = mf4_interp.compute_angdist_values(
-        endf_dict, mt, energies_in, angle_cosines_out, to_lab
-    )
-    xs = compute_xs(endf_dict, mt, energies_in).reshape(-1, 1)
-    return angdist * xs
+from .mf3_interpretation import compute_cross_section as compute_xs
 
 
 def compute_yields(endf_dict, mt, zap, energies_in):
@@ -45,6 +31,18 @@ def compute_yields(endf_dict, mt, zap, energies_in):
         mult = reaction.get_multiplicity_for_zap(proj, mt, zap)
         yields = np.full(len(energies_in), mult, dtype=float)
     return yields
+
+
+def compute_daxs(endf_dict, mt, zap, energies_in, angle_cosines_out, to_lab=True):
+    if not is_zap_consistent(endf_dict, mt, zap):
+        raise ValueError(f'MT={mt} and ZAP={mt} are not consistent')
+
+    yields = compute_yields(endf_dict, mt, zap, energies_in).reshape(-1, 1)
+    xs = compute_xs(endf_dict, mt, energies_in).reshape(-1, 1)
+    angdist = compute_angdist_values(
+        endf_dict, mt, zap, energies_in, angle_cosines_out, to_lab
+    )
+    return angdist * yields * xs / (2*np.pi)
 
 
 def compute_ddxs(endf_dict, mt, zap, energies_in, energies_out, angle_cosines_out, to_lab=True):
