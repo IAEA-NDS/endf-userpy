9c06873007f9cc91e4bf9138982ebc5f4ae12183
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-02-27 22:20:56 +0100

    implement `compute_energydist_values` func

--- endf_userpy/distribution1d.py
+++ endf_userpy/distribution1d.py
@@ -1,10 +1,12 @@
 import numpy as np
 from scipy.integrate import quad
 from . import mf4_interpretation as mf4_interp
+from . import mf5_interpretation as mf5_interp
 from .distribution2d import compute_dist2d_values
 from .properties import (
     is_zap_consistent,
     has_mf4_mt,
+    has_mf5_mt,
     has_mf6_mt,
     get_QM,
     get_QI,
@@ -39,5 +41,37 @@ def compute_angdist_values(endf_dict, mt, zap, energies_in, angle_cosines_out, t
 
     raise IndexError(
         f'Required data to reconstruct angular distribution '
+        f'for MT={mt} not available.'
+    )
+
+
+def compute_energydist_values(endf_dict, mt, zap, energies_in, energies_out, to_lab=True):
+    if not is_zap_consistent(endf_dict, mt, zap):
+        raise ValueError(f'MT={mt} and ZAP={mt} are not consistent')
+
+    if has_mf5_mt(endf_dict, mt):
+        if to_lab is not True:
+            raise ValueError(
+                f"Energy spectrum for MT={mt}, ZAP={zap} reconstruction "
+                "from MF5 only possible with `to_lab=True` argument."
+            )
+        return mf5_interp.compute_spectrum(
+            endf_dict, mt, energies_in, energies_out
+        )
+
+    elif has_mf6_mt(endf_dict, mt):
+        energydist = np.zeros((len(energies_in), len(energies_out)), dtype=float)
+        ens_inc = energies_in
+        ens_out = energies_out
+        for i in range(len(ens_inc)):
+            for j in range(len(ens_out)):
+                dist2d_func = lambda x: compute_dist2d_values(
+                    endf_dict, mt, zap, ens_inc[i:i+1], ens_out[j:j+1], np.array([x], dtype=float)
+                )
+                energydist[i, j] = quad(dist2d_func, -1.0, 1.0, epsrel=1e-4)[0]
+        return energydist
+
+    raise IndexError(
+        f'Required data to reconstruct energy spectrum '
         f'for MT={mt} not available.'
     )
