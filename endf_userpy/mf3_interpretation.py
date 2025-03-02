5f02d4c435e050a21681b5a3f36b4c7de6c8f199
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-02 15:29:49 +0100

    introduce funcs for nominal and effective incident energies

--- endf_userpy/mf3_interpretation.py
+++ endf_userpy/mf3_interpretation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from .helpers import treat_duplicates
 from .interpolation import interp_tab1
 from .properties import (
     get_projectile,
@@ -6,10 +7,30 @@ from .properties import (
 )
 
 
-def get_incident_energies(endf_dict, mt):
+def get_nominal_incident_energies(endf_dict, mt):
     sec = endf_dict[3][mt]
     xstab = sec['xstable']
-    return np.array(xstab['E'])
+    return np.array(xstab['E'], copy=True)
+
+
+def get_nominal_incident_energy_range(endf_dict, mt):
+    energies = get_nominal_incident_energies(endf_dict, mt)
+    return (min(energies), max(energies))
+
+
+def get_incident_energies(endf_dict, mt):
+    energies = get_nominal_incident_energies(endf_dict, mt)
+    treat_duplicates(energies, inplace=True)
+    xs = np.array(endf_dict[3][mt]['xstable']['xs'], dtype=float)
+    idcs = np.nonzero(xs)[0]
+    first_idx = idcs[0]-1 if idcs[0] > 0 else 0
+    last_idx = idcs[-1]+1 if idcs[-1]+1 < len(idcs) else idcs[-1]
+    return energies[first_idx:last_idx+1].copy()
+
+
+def get_incident_energy_range(endf_dict, mt):
+    eincs = get_incident_energies(endf_dict, mt)
+    return (min(eincs), max(eincs))
 
 
 def get_reaction_mts(endf_dict):
@@ -20,11 +41,6 @@ def get_reactions(endf_dict):
     mts = get_reaction_mts(endf_dict)
     reacs = [get_reaction_string_for_mt(endf_dict, m) for m in mts] 
     return reacs
-
-
-def get_incident_energy_range(endf_dict, mt):
-    eincs = get_incident_energies(endf_dict, mt)
-    return (min(eincs), max(eincs))
 
 
 def compute_cross_section(endf_dict, mt, energies_in):
