96f32693cc6af0cc6286ffbf919ab970663abb63
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-03 07:17:45 +0100

    refactor: move funcs from `mf6_interpretation` to `mf6_interpretation_subsecs`

--- endf_userpy/mfsec_interpretation/mf6_interpretation.py
+++ endf_userpy/mfsec_interpretation/mf6_interpretation.py
@@ -1,9 +1,5 @@
 import numpy as np
-from ..primitives.properties import (
-    is_zap_consistent,
-    get_ZAP,
-)
-from ..primitives.interpolation import interp_tab1
+from ..primitives.properties import get_ZAP
 from .mf6_interpretation_helpers import (
     check_mf6_exists,
     check_mt_exists_in_mf6,
@@ -11,73 +7,10 @@ from .mf6_interpretation_helpers import (
     contains_subsec_dist2d,
 )
 from .mf6_interpretation_subsecs import (
-    get_dist2d_from_subsec_law1,
-    get_dist1d_from_subsec_law2,
-    get_dist2d_from_subsec_law6,
-    get_dist2d_from_subsec_law7,
+    compute_dist2d_from_subsec,
+    compute_dist1d_from_subsec,
+    compute_yields_from_subsec,
 )
-
-
-def get_incident_energies_from_subsec(endf_dict, mt, subsec_num):
-    sec = endf_dict[6][mt]
-    subsec = sec['subsection'][subsec_num]
-    yield_tab = subsec['yields']
-    return np.array(yield_tab['Eint'], copy=True)
-
-
-def compute_yields_from_subsec(endf_dict, mt, subsec_num, energies_in):
-    sec = endf_dict[6][mt]
-    subsec = sec['subsection'][subsec_num]
-    yield_tab = subsec['yields']
-    interp_yields = interp_tab1(
-        energies_in, yield_tab, 'Eint', 'yi', outside_value=0.0
-    )
-    return interp_yields
-
-
-def compute_dist1d_from_subsec(
-    endf_dict, mt, subsec_num,
-    energies_in, angle_cosines_out, to_lab=True
-):
-    sec = endf_dict[6][mt]
-    subsec = sec['subsection'][subsec_num]
-    law = subsec['LAW']
-    if law == 2:
-        return get_dist1d_from_subsec_law2(
-            endf_dict, mt, subsec_num, energies_in, angle_cosines_out, to_lab
-        )
-    else:
-        raise NotImplementedError(
-            f'Angular distribution interpretation for LAW={law} not implemented.'
-        )
-
-
-def compute_dist2d_from_subsec(
-    endf_dict, mt, subsec_num,
-    energies_in, energies_out, angle_cosines_out, to_lab=True
-):
-    sec = endf_dict[6][mt]
-    subsec = sec['subsection'][subsec_num]
-    law = subsec['LAW']
-    if law == 1:
-        return get_dist2d_from_subsec_law1(
-            endf_dict, mt, subsec_num,
-            energies_in, energies_out, angle_cosines_out, to_lab
-        )
-    elif law == 6:
-        return get_dist2d_from_subsec_law6(
-            endf_dict, mt, subsec_num,
-            energies_in, energies_out, angle_cosines_out, to_lab
-        )
-    elif law == 7:
-        return get_dist2d_from_subsec_law7(
-            endf_dict, mt, subsec_num,
-            energies_in, energies_out, angle_cosines_out, to_lab
-        )
-    else:
-        raise NotImplementedError(
-            f'DDX interpretation for LAW={law} not implemented.'
-        )
 
 
 def get_incident_energies(endf_dict, mt, zap):
