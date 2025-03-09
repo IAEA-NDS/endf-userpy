7939d42935055609d2febacc54d6a9ae735e2e75
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-09 07:19:19 +0100

    refactor and finalize `mf12_interpretation` module

--- endf_userpy/mfsec_interpretation/mf12_interpretation.py
+++ endf_userpy/mfsec_interpretation/mf12_interpretation.py
@@ -1,109 +1,18 @@
 import numpy as np
 from ..primitives.helpers import dict2array
-from ..mfsec_interpretation import mf3_interpretation as mf3_interp
-from ..primitives import properties as prop
-from ..fortran.endf6 import (
-    trans2yield as trans2yield_fort,
-    init_trans2yield as init_trans2yield_fort,
+from ..primitives.interpolation import endf_interp1d
+from .mf12_interpretation_helpers import (
+    get_discrete_series_mts,
+    init_trans2yield,
+    trans2yield,
 )
 
 
-DISCRETE_MT_SERIES = {
-    'n': [mt for mt in range(50, 91)],
-    'p': [mt for mt in range(600, 649)],
-    'd': [mt for mt in range(650, 699)],
-    't': [mt for mt in range(700, 749)],
-    'h': [mt for mt in range(750, 799)],
-    'a': [mt for mt in range(800, 849)],
-}
-
-INV_DISCRETE_SERIES_MAP = {mt: k for k, v in DISCRETE_MT_SERIES.items() for mt in v}
-
-MAX_NUM_LEVEL = 60
-MAX_NK = 5000
-
-
-def _get_discrete_series_mts(endf_dict, mt, include_ground_state=False):
-    ejectile = INV_DISCRETE_SERIES_MAP[mt]
-    series_mts = DISCRETE_MT_SERIES[ejectile]
-    if not include_ground_state:
-        series_mts = series_mts[1:]
-    return series_mts
-
-
-def init_trans2yield(endf_dict, mt): 
-    maxlevel = MAX_NUM_LEVEL 
-    elis = prop.get_ELIS(endf_dict)
-    series_mts = _get_discrete_series_mts(
-        endf_dict, mt, include_ground_state=False
-    )
-
-    avail_mts = mf3_interp.get_reaction_mts(endf_dict) 
-    avail_series_mts = [mt for mt in series_mts if mt in avail_mts] 
-    if not np.all(np.diff(avail_series_mts) == 1):
-        raise IndexError('discrete MT number missing')
-
-    nlevel = len(avail_series_mts)
-    
-    qms = [prop.get_QM(endf_dict, mt) for mt in avail_series_mts]   
-    qis = [prop.get_QI(endf_dict, mt) for mt in avail_series_mts]
-
-    ee = np.empty(maxlevel, dtype=float, order='F')  
-    a = np.empty((maxlevel, maxlevel), dtype=float, order='F')
-    r = np.empty((maxlevel, maxlevel), dtype=float, order='F')
-
-    init_trans2yield_fort(elis, nlevel, qms, qis, ee, r, a)
-    state_cache = {
-        'ee': ee, 'r': r, 'a': a
-    }
-    return avail_series_mts, state_cache
-
-
-# subroutine trans2yield(mt,esns,nt,esi,tp,gp,maxlevel,ee,r,a,maxnk,nk,es,eg,y) ! in :endf6:endf6.f90
-def trans2yield(endf_dict, mt, state_cache):
-    maxlevel = MAX_NUM_LEVEL
-    maxnk = MAX_NK 
-
-    ee = state_cache['ee']
-    r = state_cache['r']
-    a = state_cache['a']
-
-    mtsec = endf_dict[12][mt]
-    esns = mtsec['ES_NS'] 
-    nt = len(mtsec['ES'])
-    esi = dict2array(mtsec['ES'], dtype=float, order='F')
-    tp = dict2array(mtsec['TP'], dtype=float, order='F')
-
-    if mtsec['LG'] == 1:
-        gp = np.ones(nt, dtype=float)
-    elif mtsec['LG'] == 2:
-        gp = dict2array(mtsec['GP'], dtype=float, order='F')
-    else:
-        raise ValueError(f'invalid value for LG encountered (LG={LG})')
-
-    # allocate variables for output vars from fortran subroutine
-
-    # energy level from which photon originates
-    es = np.zeros(maxnk, dtype=float, order='F') 
-    # photon energy 
-    eg = np.zeros(maxnk, dtype=float, order='F')
-    # photon yield
-    y = np.zeros(maxnk, dtype=float, order='F')
-    # number of transitions
-    nko = np.empty(1, dtype=np.int64)
-
-    # call fortran subroutine
-    trans2yield_fort(mt, esns, nt, esi, tp, gp, ee, r, a, maxnk, nko, es, eg, y)
-    nk = int(nko.item())
-    return {
-        'level_energy': es[:nk],
-        'photon_energy': eg[:nk],
-        'photon_yield': y[:nk],
-    }
-
-
-def compute_cascade_photon_yields(endf_dict, mts):
-    series_mts = _get_discrete_series_mts(
+def compute_photon_yields_from_transition_probabilities(endf_dict, mts):
+    scalar_mt = not hasattr(mts, '__iter__')
+    if scalar_mt:
+        mts = [mts]
+    series_mts = get_discrete_series_mts(
         endf_dict, mts[0], include_ground_state=False
     )
     if not np.all(np.isin(mts, series_mts)):
@@ -126,4 +35,55 @@ def compute_cascade_photon_yields(endf_dict, mts):
             results[mt] = cur_result
             if mt == user_max_mt:
                 break
+    if scalar_mt:
+        results = results[mts[0]]
     return results
+
+
+def compute_photon_yields_from_tabulated_yields(endf_dict, mt, energies_in):
+    mtsec = endf_dict[12][mt]
+    if mtsec['LO'] != 1:
+        raise ValueError(
+            f'MT{mt} does not contain photon multiplicities'
+        )
+    eincs = energies_in
+    num_eincs = len(energies_in)
+    num_secs = mtsec['NK']
+    tables = list(mtsec['table'].values())
+
+    level_energies = dict2array(mtsec['ES']) 
+    photon_energies = dict2array(mtsec['Eg'])
+    photon_yields = np.array(
+        [
+            endf_interp1d(
+            eincs, t['Eint'], t['y'], t['INT'], t['NBT'], outside_value=0.0
+            ) for t in tables
+        ]
+    ).T
+    return {
+        'level_energy': level_energies, 
+        'photon_energy': photon_energies,
+        'photon_yield': photon_yields,
+    }
+
+
+def compute_photon_yields(endf_dict, mt, energies_in):
+    mtsec = endf_dict[12][mt]
+    LO_value = mtsec['LO']
+    if LO_value == 1:
+        return compute_photon_yields_from_tabulated_yields(
+            endf_dict, mt, energies_in
+        )
+    elif LO_value == 2:
+        res = compute_photon_yields_from_transition_probabilities(
+            endf_dict, mt
+        )
+        ones_vec = np.ones_like(energies_in).reshape(-1, 1)
+        res['photon_yield'] = (
+            res['photon_yield'].reshape(1,-1) * ones_vec
+        )
+        return res
+    else:
+        raise ValueError(
+            'Invalid value LO={LO_value}'
+        )
