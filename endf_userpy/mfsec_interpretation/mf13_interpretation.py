5560f808ff436cc8936681045210e1293a27e3f8
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-10 22:12:12 +0100

    change interface in mf13 interpretation
    don't return the labels (e.g. photon energy and
    levels) but only the result, e.g. production cross
    section, and in the appropriate shape. For instance,
    if energies_in (length M)  and photon_energies (length N)
    arrays are passed, return an M x N array with corresponding
    cross sections.

--- endf_userpy/mfsec_interpretation/mf13_interpretation.py
+++ endf_userpy/mfsec_interpretation/mf13_interpretation.py
@@ -1,5 +1,8 @@
 import numpy as np
-from ..primitives.helpers import dict2array
+from ..primitives.helpers import (
+    dict2array,
+    find_indices_with_tol,
+)
 from ..primitives.interpolation import endf_interp1d
 
 
@@ -20,12 +23,21 @@ def _compute_total_production_xs(endf_dict, mt, energies_in):
     )
 
 
-def compute_photon_production_xs(endf_dict, mt, energies_in):
+def compute_photon_production_xs(endf_dict, mt, energies_in, photon_energies):
     mtsec = endf_dict[13][mt]
     eincs = energies_in
     subsecs = list(mtsec['subsection'].values())
     level_energies = np.array([sec['ES'] for sec in subsecs])
-    photon_energies = np.array([sec['EG'] for sec in subsecs])
+    existing_photon_energies = np.array([sec['EG'] for sec in subsecs])
+    idcs = find_indices_with_tol(
+        existing_photon_energies, photon_energies, atol=1e-3, rtol=1e-5
+    )
+    if np.any(idcs == -1):
+        raise ValueError(
+            'Array `photon_energies` must only contain energies '
+            'that are also present in the file.'
+        )
+    subsecs = [subsecs[idx] for idx in idcs]
     prod_xs = np.array(
         [
             endf_interp1d(
@@ -33,16 +45,14 @@ def compute_photon_production_xs(endf_dict, mt, energies_in):
             ) for t in subsecs
         ]
     ).T
-    return {
-        'level_energy': level_energies,
-        'photon_energy': photon_energies,
-        'photon_prodxs': prod_xs
-    }
+    return prod_xs
 
 
 def compute_total_photon_production_xs(endf_dict, mt, energies_in):
-    prod = compute_photon_production_xs(endf_dict, mt, energies_in)
-    prod_xs = prod['photon_prodxs']
+    photon_energies = get_photon_energies(endf_dict, mt)
+    prod_xs = compute_photon_production_xs(
+        endf_dict, mt, energies_in, photon_energies
+    )
     total_prod_xs = np.sum(prod_xs, axis=1)
     if prod_xs.shape[0] > 1:
         check_tot_prod_xs = _compute_total_production_xs(
