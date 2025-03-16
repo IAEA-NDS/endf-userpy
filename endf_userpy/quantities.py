8fc1729968ab8451fdfd9ebf34eacc61ae75e4f6
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-16 09:29:40 +0100

    simplify `get_reaction_xs` by using `compute_xs_mt5_contrib`

--- endf_userpy/quantities.py
+++ endf_userpy/quantities.py
@@ -5,6 +5,7 @@ from .primitives import reactions as reac
 from .quantities_mt_zap.quantities import (
     compute_yields,
     compute_xs,
+    compute_xs_mt5_contrib,
     compute_dexs,
     compute_daxs,
     get_reaction_mt_numbers,
@@ -32,39 +33,9 @@ def get_available_reactions(endf_dict):
 def get_reaction_xs(endf_dict, reaction, energies_in):
     proj = prop.get_projectile(endf_dict)
     mt = reac.translate_reaction_string_to_mt(reaction)
-    simple_xs = compute_xs(endf_dict, mt, energies_in)
-
-    # determine whether combination with MT5 is possible
-    ejectiles = reac.get_ejectiles(proj, mt)
-    is_simple_xs = ejectiles is None
-    is_simple_xs |= reac.is_discrete_level_scattering(mt)
-    if is_simple_xs:
-        return simple_xs
-    is_simple_xs |= len(ejectiles) > 1
-    if is_simple_xs:
-        return simple_xs
-    ejectile = ejectiles[0][1]
-    is_simple_xs |= ejectile not in ('n', 'p')
-    if is_simple_xs:
-        return simple_xs
-
-    # determine corresponding residual product
-    za_projectile = prop.get_ZAI(endf_dict)
-    za_target = prop.get_ZA(endf_dict)
-    za_ejectile = physconst.get_zap_for_particle(ejectile)
-    m = reac.get_multiplicity_for_zap(endf_dict, mt, za_ejectile)
-    za_residual = za_target + za_projectile - m * za_ejectile
-
-    # reconstruct cross section component from mf6/mt5
-    einc_sel = (simple_xs == 0.0)
-    einc_reduced = energies_in[einc_sel]
-    yield_mt5 = compute_yields(
-        endf_dict, 5, za_residual, einc_reduced, include_discrete=True
-    )
-    xs_mt5 = compute_xs(endf_dict, 5, einc_reduced)
-    xs_add = xs_mt5 * yield_mt5
-    simple_xs[einc_sel] += xs_add
-    return simple_xs
+    xs = compute_xs(endf_dict, mt, energies_in)
+    xs_mt5 = compute_xs_mt5_contrib(endf_dict, mt, energies_in)
+    return xs + xs_mt5
 
 
 def get_particle_production_xs(endf_dict, particle, energies_in):
