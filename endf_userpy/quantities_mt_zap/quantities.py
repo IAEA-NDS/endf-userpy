fcab2f38803a81ffaf7ab896958ac3265b113c6d
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-16 09:28:49 +0100

    implement `compute_xs_mt6_contrib` func

--- endf_userpy/quantities_mt_zap/quantities.py
+++ endf_userpy/quantities_mt_zap/quantities.py
@@ -13,6 +13,7 @@ from .distribution1d import (
     compute_angdist_values,
     compute_energydist_values,
 )
+from ..mfsec_interpretation import mf6_interpretation_helpers as mf6_help
 from .distribution2d import compute_dist2d_values
 
 # functions borrowed as is
@@ -43,6 +44,32 @@ def compute_yields(endf_dict, mt, zap, energies_in, include_discrete=True):
         mult = reaction.get_multiplicity_for_zap(proj, mt, zap)
         yields = np.full(len(energies_in), mult, dtype=float)
     return yields
+
+
+def compute_xs_mt5_contrib(endf_dict, mt, energies_in):
+    if not has_mf6_mt(endf_dict, 5):
+        return np.zeros_like(energies_in, dtype=float)
+    proj = properties.get_projectile(endf_dict)
+    ejectiles = reaction.get_ejectiles(proj, mt)
+    if ejectiles is None or len(ejectiles) > 1:
+        return np.zeros_like(energies_in, dtype=float)
+    ejectile = ejectiles[0][1]
+    if ejectile not in ('n', 'p'):
+        return np.zeros_like(energies_in, dtype=float)
+
+    za_projectile = properties.get_ZAI(endf_dict)
+    za_target = properties.get_ZA(endf_dict)
+    za_ejectile = get_zap_for_particle(ejectile)
+    m = reaction.get_multiplicity_for_zap(endf_dict, mt, za_ejectile)
+    za_residual = za_target + za_projectile - m * za_ejectile
+    if not mf6_help.has_subsecs_for_mt_zap(endf_dict, mt, za_residual):
+        return np.zeros_like(energies_in, dtype=float)
+
+    yield_mt5 = compute_yields(
+        endf_dict, 5, za_residual, energies_in, include_discrete=True
+    )
+    xs_mt5 = compute_xs(endf_dict, 5, energies_in)
+    return xs_mt5 * yield_mt5
 
 
 def compute_daxs(endf_dict, mt, zap, energies_in, angle_cosines_out, to_lab=True):
