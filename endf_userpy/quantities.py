3c7c2571a559e1e1c6adb1e342d316b7f128333a
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-19 20:51:57 +0100

    import modules rather than individual functions

--- endf_userpy/quantities.py
+++ endf_userpy/quantities.py
@@ -2,26 +2,16 @@ import numpy as np
 from .primitives import physical_constants as physconst
 from .primitives import properties as prop
 from .primitives import reactions as reac
-from .quantities_mt_zap.quantities import (
-    compute_yields,
-    compute_xs,
-    compute_dexs,
-    compute_daxs,
-    get_reaction_mt_numbers,
-    compute_cumulative_quantity,
-)
+from .quantities_mt_zap import quantities as quant_mt_zap
 from .quantities_mt_zap import distribution1d as dist1d
-from .quantities_mt_zap.selectors import (
-    contains_zap,
-    satisfies_select_heuristic,
-)
+from .quantities_mt_zap import selectors
 
 
 # TODO: check and complete
 
 
 def get_available_reactions(endf_dict):
-    mts = get_reaction_mt_numbers(endf_dict)
+    mts = quant_mt_zap.get_reaction_mt_numbers(endf_dict)
     reacs = [
         prop.get_reaction_string_for_mt(endf_dict, mt)
         for mt in mts
@@ -31,20 +21,20 @@ def get_available_reactions(endf_dict):
 
 def get_reaction_xs(endf_dict, reaction, energies_in, mt5_contrib=True):
     mt = reac.translate_reaction_string_to_mt(reaction)
-    return compute_xs(endf_dict, mt, energies_in, mt5_contrib)
+    return quant_mt_zap.compute_xs(endf_dict, mt, energies_in, mt5_contrib)
 
 
 def get_particle_production_xs(endf_dict, particle, energies_in):
     zap = physconst.get_zap_for_particle(particle)
-    return compute_cumulative_quantity(
+    return quant_mt_zap.compute_cumulative_quantity(
         lambda endf_dict, mt, zap, energies_in: (
-            compute_yields(
+            quant_mt_zap.compute_yields(
                 endf_dict, mt, zap, energies_in, include_discrete=True
-            ) * compute_xs(endf_dict, mt, energies_in)
+            ) * quant_mt_zap.compute_xs(endf_dict, mt, energies_in)
         ),
         lambda endf_dict, mt, zap, energies_in: (
-            contains_zap(endf_dict, mt, zap) and
-            satisfies_select_heuristic(endf_dict, mt)
+            selectors.contains_zap(endf_dict, mt, zap) and
+            selectors.satisfies_select_heuristic(endf_dict, mt)
         ),
         endf_dict, zap, energies_in
     )
@@ -52,13 +42,13 @@ def get_particle_production_xs(endf_dict, particle, energies_in):
 
 def get_particle_production_dxs_dE(endf_dict, particle, energies_in, energies_out):
     zap = physconst.get_zap_for_particle(particle)
-    return compute_cumulative_quantity(
+    return quant_mt_zap.compute_cumulative_quantity(
         lambda endf_dict, mt, zap, energies_in, energies_out: (
-            compute_dexs(endf_dict, mt, zap, energies_in, energies_out)
+            quant_mt_zap.compute_dexs(endf_dict, mt, zap, energies_in, energies_out)
         ),
         lambda endf_dict, mt, zap, energies_in, energies_out: (
-            contains_zap(endf_dict, mt, zap) and
-            satisfies_select_heuristic(endf_dict, mt)
+            selectors.contains_zap(endf_dict, mt, zap) and
+            selectors.satisfies_select_heuristic(endf_dict, mt)
         ),
         endf_dict, zap, energies_in, energies_out
     )
@@ -66,13 +56,13 @@ def get_particle_production_dxs_dE(endf_dict, particle, energies_in, energies_ou
 
 def get_particle_production_dxs_dmu(endf_dict, particle, energies_in, angle_cosines_out):
     zap = physconst.get_zap_for_particle(particle)
-    return compute_cumulative_quantity(
+    return quant_mt_zap.compute_cumulative_quantity(
         lambda endf_dict, mt, zap, energies_in, angle_cosines_out: (
-            compute_daxs(endf_dict, mt, zap, energies_in, angle_cosines_out, to_lab=True)
+            quant_mt_zap.compute_daxs(endf_dict, mt, zap, energies_in, angle_cosines_out, to_lab=True)
         ),
         lambda endf_dict, mt, zap, energies_in, energies_out: (
-            contains_zap(endf_dict, mt, zap) and
-            satisfies_select_heuristic(endf_dict, mt)
+            selectors.contains_zap(endf_dict, mt, zap) and
+            selectors.satisfies_select_heuristic(endf_dict, mt)
         ),
         endf_dict, zap, energies_in, angle_cosines_out
     )
