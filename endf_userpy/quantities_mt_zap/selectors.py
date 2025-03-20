d527cffbfd4151c60282f2dcb84fa99d22db57a4
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-20 13:35:27 +0100

    fix/refine `contains_zap` selector

--- endf_userpy/quantities_mt_zap/selectors.py
+++ endf_userpy/quantities_mt_zap/selectors.py
@@ -11,12 +11,28 @@ from .quantities import (
 def contains_zap(endf_dict, mt, zap):
     if mt in (18, 19, 20, 21, 38):
         return zap == physconst.PARTICLE_ZAP['n']
-    elif prop.has_mf6_mt(endf_dict, mt):
+    if prop.has_mf6_mt(endf_dict, mt):
         return mf6help.contains_zap(endf_dict, mt, zap)
-    else:
-        projectile = prop.get_projectile(endf_dict)
-        ret = reac.contains_zap(projectile, mt, zap)
-        return ret if ret is not None else False
+
+    projectile = prop.get_projectile(endf_dict)
+    ret = reac.contains_zap(projectile, mt, zap)
+    if ret is True:
+        return True
+    if ret is None and not reac.is_sum_mt(mt):
+        raise ValueError(
+            "Unable to determine whether reaction associated with "
+            f"MT={mt} contains a particle identified by ZAP={zap}."
+        )
+    # for some sum channels, it can't be determined
+    # from the reaction string whether there is a zap ejectile
+    # and we need to loop over the available partial mts to figure out.
+    avail_mts = get_reaction_mt_numbers(endf_dict)
+    part_mts = reac.get_part_mts_from_sum_mt(mt)
+    loop_mts = set(avail_mts).intersection(part_mts)
+    for part_mt in loop_mts:
+        if contains_zap(projectile, part_mt, zap):
+            return True
+    return False
 
 
 def satisfies_select_heuristic(endf_dict, mt, user_mts=None):
